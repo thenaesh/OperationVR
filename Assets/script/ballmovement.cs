dbca28690a7f4d78ce99c0e0bc04d2fad34bb92c
Thenaesh Elango <thenaeshelango@gmail.com>
2016-05-21 16:00:10 +0800

    first commit, project set up

---
+++ Assets/script/ballmovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballmovement : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        Vector3 movement = new Vector3(0.0f, 0.0f, speed);
+        rb.AddForce(movement * speed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
