4648994d4082da52059b86754cd9310cc1495ac6
00014on <00014on@gmail.com>
2016-05-26 14:26:33 +0800

    add environment graphics, toggle by clicking when looking at the hand.

---
+++ Assets/ToggleEnvironment.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleEnvironment : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void EnvironmentToggle()
+    {
+         GameObject EnvironmentParent = GameObject.Find("EnvironmentParent");
+         GameObject Island = EnvironmentParent.transform.Find("IslandEnvironmentPrefab").gameObject;
+        
+
+        if (Island.activeSelf == true)
+        {
+            Island.SetActive(false);
+        }
+
+        else 
+        {
+            Island.SetActive(true);
+        }
+            }
+}
+
