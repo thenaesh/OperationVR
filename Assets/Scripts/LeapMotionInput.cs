9ba2a97a429792241ab70198de59e39138982f7e
Thenaesh Elango <thenaeshelango@gmail.com>
2016-06-07 18:29:07 +0800

    add skeleton for Leap Motion input handler

---
+++ Assets/Scripts/LeapMotionInput.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class LeapMotionInput : MonoBehaviour
+{
+	public const int recvPort = 10000;
+	private UdpClient recv;
+	private IPEndPoint ep;
+
+	void Start()
+	{
+		recv = new UdpClient(recvPort);
+		ep = new IPEndPoint(IPAddress.Any, recvPort);
+	}
+
+	void Update()
+	{
+		try
+		{
+			byte[] recvBytes = recv.Receive(ref ep);
+			modifyTransformProperties(recvBytes);
+		}
+		catch (Exception e)
+		{
+		}
+		finally
+		{
+			recv.Close();
+		}
+	}
+
+	void modifyTransformProperties(byte[] data)
+	{
+	}
+}
+
