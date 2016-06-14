c8fd662df267d73f5ac89ec3ea3388c7fa555971
mike <weijianneo@hotmail.com>
2016-06-14 20:42:55 +0800

    add proper start screen. PROB: only host can launch the ball, goal detection is processed locally and client may miss out a score - try to remedy using varsync.

---
+++ Assets/Scripts/sb_NetworkManagerInterface.cs
@@ -0,0 +1,60 @@
+﻿#if ENABLE_UNET
+using UnityEngine.UI;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using System.Collections;
+
+namespace UnityEngine.Networking
+{
+	[AddComponentMenu("Network/NetworkManagerHUD")]
+	[RequireComponent(typeof(NetworkManager))]
+	[System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+	public class sb_NetworkManagerInterface : MonoBehaviour {
+		private NetworkManager manager;
+
+		public Text inputfield;
+		public Text outText;
+
+		void Awake()
+		{
+			manager = GetComponent<NetworkManager>();
+		}
+
+		void Update()
+		{
+			outText.text = "My IP Address: " +  LocalIPAddress();
+		}
+
+		public void ConnectButtonPress() {
+			Debug.Log (inputfield.text);
+			manager.networkAddress = inputfield.text;
+			manager.StartClient();
+		}
+
+
+		public void HostButtonPress() {
+			manager.StartHost();
+		}
+	
+	
+		public string LocalIPAddress()
+		{
+			IPHostEntry host;
+			string localIP = "";
+			host = Dns.GetHostEntry(Dns.GetHostName());
+			foreach (IPAddress ip in host.AddressList)
+			{
+				if (ip.AddressFamily == AddressFamily.InterNetwork)
+				{
+					localIP = ip.ToString();
+					break;
+				}
+			}
+			return localIP;
+		}
+	}
+		
+}
+
+#endif //ENABLE_UNET
