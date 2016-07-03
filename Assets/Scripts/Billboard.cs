1c4aa54ca6475bbf35dd04339eb5ef8c93c0c320
mike <weijianneo@hotmail.com>
2016-07-03 17:51:09 +0800

    fix scoreboard orientation by adding public camera field

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 
 public class Billboard : MonoBehaviour {
+	public Camera mainCamera;
 
 	void Update () {
 		try
 		{
 			//transform.LookAt(Camera.main.transform); //text is inverted
-			transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+			transform.rotation = Quaternion.LookRotation(transform.position - mainCamera.transform.position);
 		} catch {
 			Debug.Log ("Changing camera: " + Time.time);
 		}
